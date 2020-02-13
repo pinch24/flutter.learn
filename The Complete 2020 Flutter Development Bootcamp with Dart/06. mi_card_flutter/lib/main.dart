5c3efd15a818bde802a0b007b1c65751bd7996c5
mk <pinch24@gmail.com>
2020-02-13 23:52:46 +0900

    06.43 mi_card_flutter

--- The Complete 2020 Flutter Development Bootcamp with Dart/06. mi_card_flutter/lib/main.dart
+++ The Complete 2020 Flutter Development Bootcamp with Dart/06. mi_card_flutter/lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
             child: Container(
               height: 100.0,
               width: 100.0,
-              margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+              margin: EdgeInsets.only(left: 30.0),
+              padding: EdgeInsets.all(20.0),
               color: Colors.white,
               child: Text('Hello'),
             ),
